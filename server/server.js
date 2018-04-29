8b1bd6d1664e0fbf052a2b0dc6cda6cca35e9e93
bhargavi.dn <bhargavi.bh92@gmail.com>
2018-04-29 09:18:24 +0530

    added emit events

--- server/server.js
+++ server/server.js
@@ -1,12 +1,34 @@
 const express=require("express");
 const app=express();
 const path=require("path");
+const socketIO=require("socket.io");
+const http=require('http');
 
 const port=process.env.PORT || 3000;
 
 var publicPath=path.join(__dirname,"../public");
+var server=http.createServer(app);
+var io=socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on("connection",(socket)=>{
+  console.log("New User connected");
+
+    socket.on("createMessage",(message)=>{
+      //console.log("createMessage ",message);
+      io.emit("newMessage",{
+        from:message.from,
+        text:message.text,
+        createdAt:new Date().getTime()
+      })
+    })
+  socket.on('disconnect',(socket)=>{
+    console.log("Client disconnected");
+  });
+
+});
+
+server.listen(port,()=>{
   console.log("Server started");
 })
